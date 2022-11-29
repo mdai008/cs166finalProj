b4cbcd01e5bf7de2a823ecf71d9f289a8d80906e
Michael Dai <mdai008@ucr.edu>
2022-11-28 21:40:30 -0800

    add scripts

--- project/sql/src/load_data.sql
+++ project/sql/src/load_data.sql
@@ -1,32 +1,32 @@
 COPY Users
-FROM 'users.csv'
+FROM '/home/user/project/data/users.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE users_userID_seq RESTART 101;
 
 COPY Store
-FROM 'stores.csv'
+FROM '/home/user/project/data/stores.csv'
 WITH DELIMITER ',' CSV HEADER;
 
 COPY Product
-FROM 'products.csv'
+FROM '/home/user/project/data/products.csv'
 WITH DELIMITER ',' CSV HEADER;
 
 COPY Warehouse
-FROM 'warehouse.csv'
+FROM '/home/user/project/data/warehouse.csv'
 WITH DELIMITER ',' CSV HEADER;
 
 COPY Orders
-FROM 'orders.csv'
+FROM '/home/user/project/data/orders.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE orders_orderNumber_seq RESTART 501;
 
 
 COPY ProductSupplyRequests
-FROM 'productSupplyRequests.csv'
+FROM '/home/user/project/data/productSupplyRequests.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE productsupplyrequests_requestNumber_seq RESTART 11;
 
 COPY ProductUpdates
-FROM 'productUpdates.csv'
+FROM '/home/user/project/data/productUpdates.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE productupdates_updateNumber_seq RESTART 51;
