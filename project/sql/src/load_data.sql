13a578311c082d4137e00d28fc00dc378719ea8a
Michael Dai <mdai008@ucr.edu>
2022-11-28 21:53:13 -0800

    update data path

--- project/sql/src/load_data.sql
+++ project/sql/src/load_data.sql
@@ -1,32 +1,32 @@
 COPY Users
-FROM '/home/user/project/data/users.csv'
+FROM '/extra/mdai008/cs166/finalProj/cs166finalProj/project/data/users.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE users_userID_seq RESTART 101;
 
 COPY Store
-FROM '/home/user/project/data/stores.csv'
+FROM '/extra/mdai008/cs166/finalProj/cs166finalProj/project/data/stores.csv'
 WITH DELIMITER ',' CSV HEADER;
 
 COPY Product
-FROM '/home/user/project/data/products.csv'
+FROM '/extra/mdai008/cs166/finalProj/cs166finalProj/project/data/products.csv'
 WITH DELIMITER ',' CSV HEADER;
 
 COPY Warehouse
-FROM '/home/user/project/data/warehouse.csv'
+FROM '/extra/mdai008/cs166/finalProj/cs166finalProj/project/data/warehouse.csv'
 WITH DELIMITER ',' CSV HEADER;
 
 COPY Orders
-FROM '/home/user/project/data/orders.csv'
+FROM '/extra/mdai008/cs166/finalProj/cs166finalProj/project/data/orders.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE orders_orderNumber_seq RESTART 501;
 
 
 COPY ProductSupplyRequests
-FROM '/home/user/project/data/productSupplyRequests.csv'
+FROM '/extra/mdai008/cs166/finalProj/cs166finalProj/project/data/productSupplyRequests.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE productsupplyrequests_requestNumber_seq RESTART 11;
 
 COPY ProductUpdates
-FROM '/home/user/project/data/productUpdates.csv'
+FROM '/extra/mdai008/cs166/finalProj/cs166finalProj/project/data/productUpdates.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE productupdates_updateNumber_seq RESTART 51;
