bbc33ab4f7eb1386c468d82503ca9c81e33fbefa
Michael Dai <mdai008@ucr.edu>
2022-12-05 23:15:39 -0800

    add timestamp trigger

--- project/sql/src/triggers.sql
+++ project/sql/src/triggers.sql
@@ -1,30 +1,30 @@
--- CREATE OR REPLACE LANGUAGE plpgsql;
+CREATE OR REPLACE LANGUAGE plpgsql;
 
 -- --create sequence
 -- DROP SEQUENCE IF EXISTS userIDseq;
 -- CREATE SEQUENCE userIDseq START WITH 101;
 
--- --define trigger function
--- CREATE OR REPLACE FUNCTION fillUserID()
--- RETURNS "trigger" AS
--- $BODY$
--- BEGIN
---     NEW.userID = nextval('userIDseq'); 
---     RETURN NEW; 
+--define before trigger function for setting orders timestamp
+CREATE OR REPLACE FUNCTION setTimestamp()
+RETURNS "trigger" AS
+$BODY$
+BEGIN
+    NEW.orderTime = DATE_TRUNC('second', CURRENT_TIMESTAMP::timestamp); 
+    RETURN NEW; 
 
 
--- END;
--- $BODY$
--- LANGUAGE plpgsql VOLATILE;
+END;
+$BODY$
+LANGUAGE plpgsql VOLATILE;
 
 
--- --create trigger
--- DROP TRIGGER IF EXISTS userIDTrigger ON Users;
+--create setTimestamp trigger
+DROP TRIGGER IF EXISTS timestampTrigger ON Orders;
 
--- CREATE TRIGGER userIDTrigger
--- BEFORE INSERT
--- ON Users
--- FOR EACH ROW
--- EXECUTE PROCEDURE fillUserID();
+CREATE TRIGGER timestampTrigger
+BEFORE INSERT
+ON Orders
+FOR EACH ROW
+EXECUTE PROCEDURE setTimestamp();
 
 
